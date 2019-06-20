633bbec3df10250ffeb6388b9a919b670037dabb
Morgan JANDER <mjander@c3r6s6.wethinkcode.co.za>
2019-06-20 13:07:50 +0200

    modified header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: mjander <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 10:54:04 by mjander           #+#    #+#             */
-/*   Updated: 2019/06/20 10:54:11 by mjander          ###   ########.fr       */
+/*   Updated: 2019/06/20 11:03:01 by mjander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define	GET_NEXT_LINE_H
 
+#include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#define BUFF_SIZE 32
+
+#endif
