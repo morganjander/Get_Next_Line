679bf04166c39d2413657402a81339444beb48b5
Morgan JANDER <mjander@c3r5s1.wethinkcode.co.za>
2019-06-29 13:17:00 +0200

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,31 @@
 /*   By: mjander <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 10:52:13 by mjander           #+#    #+#             */
-/*   Updated: 2019/06/20 10:53:33 by mjander          ###   ########.fr       */
+/*   Updated: 2019/06/29 13:15:33 by mjander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
 
-int	get_next_line(const char fd, char **line)
+static t_list	*getfile(t_list **file, int fd)
+{
+	t_list	*tmp;
+
+	tmp = *file;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("/0", fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
+}
+
+int				get_next_line(const char fd, char **line)
 {
 	return (0);
 }
